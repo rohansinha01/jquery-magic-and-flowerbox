a6fa134a857d58a1730761992188975aa55ac2ff
rohansinha01 <rohansinha01@gmail.com>
2023-11-25 19:53:39 -0500

    Commit 2 - I added more elements. My power grows with my dedication and hard work

--- hogwarts/app.js
+++ hogwarts/app.js
@@ -11,4 +11,21 @@ const $h1 = $('<h1>')
 $container.append($h1)
 $h1.text('Hogwarts')
 
+const $name = $('<h2>')
+$container.append($name)
+$name.text('Rohan Sinha')
+
+const $house = $('<h3>')
+$container.append($house)
+$house.text('Gryffindor')
+
+const $pet = $('<h4>')
+$container.append($pet)
+$pet.text('Freddy')
+$pet.addClass('owl')
+
+const $wand = $('<h4>')
+$container.append($wand)
+$wand.text('Hawthorn Wand with Dragon Heartstring Core')
+
 });
