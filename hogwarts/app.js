8849ef92761a849c158034cabe7319bbe35bafd0
rohansinha01 <rohansinha01@gmail.com>
2023-11-25 19:20:51 -0500

    Commit 1 - I added my first element and it was fun

--- hogwarts/app.js
+++ hogwarts/app.js
@@ -1,6 +1,14 @@
 
-if (typeof jQuery == 'undefined'){
-    console.log('oops! I still have to link my jQuery properly!');
-  } else {console.log('I did it! I linked jQuery and this js file!')};
-  
-  
+// if (typeof jQuery == 'undefined'){
+//     console.log('oops! I still have to link my jQuery properly!');
+//   } else {console.log('I did it! I linked jQuery and this js file!')};
+$(() => {
+    const $container = $('#container');	// put jQuery in here
+    console.log($container)
+
+
+const $h1 = $('<h1>')
+$container.append($h1)
+$h1.text('Hogwarts')
+
+});
